71bca004e8d67c12e7bb90a8fb6ec653f989e0a3
NavoAma-Perera <pereranavo20011212@gmail.com>
2025-08-17 17:47:21 +0530

    enhanced

--- src/app/(mainFolder)/home/(components)/contact.tsx
+++ src/app/(mainFolder)/home/(components)/contact.tsx
@@ -62,8 +62,7 @@ export default function Contact() {
       >
         <Send size={28} className="text-white" />
       <h2 className="text-2xl sm:text-3xl md:text-4xl font-bold text-white">
-  Let's <span className="bg-gradient-to-r from-[#FFD300] via-yellow-300 to-yellow-500 bg-clip-text text-transparent">Connect</span> & 
- <span className="bg-gradient-to-r from-[#FFD300] via-yellow-300 to-yellow-500 bg-clip-text text-transparent"> Create</span>
+  Get in <span className="bg-gradient-to-r from-[#FFD300] via-yellow-300 to-yellow-500 bg-clip-text text-transparent">Touch</span> & 
 
 </h2>
       </motion.h2>
